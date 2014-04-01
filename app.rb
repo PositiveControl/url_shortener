b86f4b5c310f0d55fd11b9f74c6a48110851324e
Mark Evans <twenty4play@gmail.com>
2014-04-01 15:20:22 -0600

    [Delivers #68381280]  URL is shortened and displayed with orignal URL
    Added CSS styles to index and results to match mockups.
    Refactored test to be more readable.
    App now displays 'shortened' URL along with old URL.
    
    Signed-off-by: Mike Kauffman <mike.a.kauffman@gmail.com>

--- app.rb
+++ app.rb
@@ -10,13 +10,14 @@ class App < Sinatra::Application
 
   post '/urls' do
     new_url = Hash.new
-    new_url[:id] = STORED_URLS.length.to_i + 1
+    new_url[:id] = STORED_URLS.length + 1
     new_url[:entered_url] = params[:url_entry]
-    STORED_URLS.push(new_url)
+    STORED_URLS << new_url
     redirect ('/urls')
   end
 
   get '/urls' do
-    erb :results, locals: {urls: STORED_URLS}
+    urlid = STORED_URLS.length - 1
+    erb :results, locals: {urls: STORED_URLS, url_id: urlid }
   end
 end
--- spec/index_spec.rb
+++ spec/index_spec.rb
@@ -4,11 +4,9 @@ require_relative '../app'
 
 Capybara.app = App
 
-feature 'finding the homepage' do
-
-  scenario 'homepage has a form' do
+feature 'Shortens a URL' do
+  scenario 'homepage has a form and button' do
     visit ('/')
-    expect(find_field('url_entry').value).to eq 'Enter the URL you would like to "shorten"'
     fill_in "url_entry", :with => "http://www.whitehouse.com"
     click_on "Shorten"
   end
