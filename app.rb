1d5732766966950d73fe1ae4699902b1e5d52e24
Mark Evans <twenty4play@gmail.com>
2014-04-02 14:28:51 -0600

    [Delivers #68381290] Error message for invalid URL added
    User is prompted with an error message when entering an invalid URL.
    
    Signed-off-by: Mike Kauffman <mike.a.kauffman@gmail.com>

--- app.rb
+++ app.rb
@@ -1,30 +1,43 @@
 require 'sinatra/base'
+require 'uri'
 
 class App < Sinatra::Application
 
   STORED_URLS = Array.new
+  ERRORMESSAGE = []
 
   get '/' do
-    erb :index
+    erb :index, locals: {error_message: ERRORMESSAGE}
   end
 
   post '/urls' do
-    new_url = Hash.new
-    new_url[:id] = STORED_URLS.length + 1
-    new_url[:entered_url] = params[:url_entry]
-    STORED_URLS << new_url
-    redirect ('/urls')
+
+    if (params[:url_entry] =~ /^#{URI::regexp}$/) == nil
+      ERRORMESSAGE << "The text you entered is not a valid URL"
+      redirect ('/')
+    else
+      new_url = Hash.new
+      new_url[:id] = STORED_URLS.length + 1
+      new_url[:entered_url] = params[:url_entry]
+      STORED_URLS << new_url
+      ERRORMESSAGE.clear
+      redirect ('/urls')
+    end
   end
 
   get '/urls' do
     urlid = STORED_URLS.length - 1
-    erb :results, locals: {urls: STORED_URLS, url_id: urlid }
+    erb :results, locals: {urls: STORED_URLS, url_id: urlid}
   end
 
   get '/:id' do
     url_acces = params[:id].to_i - 1
     orig_url = STORED_URLS[url_acces][:entered_url]
-    redirect 'http://' + orig_url
+    redirect orig_url
+  end
+
+  get '/urls/invalid' do
+    erb :invalid_url
   end
 
 end
--- spec/index_spec.rb
+++ spec/index_spec.rb
@@ -7,20 +7,32 @@ Capybara.app = App
 feature 'Shortens a URL' do
   scenario 'homepage has a form and button' do
     visit ('/')
-    fill_in "url_entry", :with => "www.google.com"
+    fill_in "url_entry", :with => "http://www.google.com"
     click_on "Shorten"
   end
 
   scenario 'user submits url and is returned a shortened version' do
     visit ('/')
-    fill_in "url_entry", :with => "www.google.com"
+    fill_in "url_entry", :with => "http://www.google.com"
     click_on "Shorten"
     expect(page).to have_content "Original URL"
     expect(page).to have_content '"Shortened" URL'
-    expect(page).to have_content "www.google.com"
+    expect(page).to have_content "http://www.google.com"
     expect(page).to have_content "http://ur-ly.herokuapp.com/2"
 
     visit '/2'
     current_url.should == "http://www.google.com/"
+  end
+
+  scenario 'user inputs non-URL' do
+    visit ('/')
+    fill_in "url_entry", :with => "www.google.com"
+    click_on "Shorten"
+    expect(page).to have_content "The text you entered is not a valid URL"
+    fill_in "url_entry", :with => "http://www.google.com"
+    click_on "Shorten"
+    expect(page).to have_content "Original URL"
+    visit ('/')
+    expect(page).to_not have_content "The text you entered is not a valid URL"
   end
 end
